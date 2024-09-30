b698632aee7a6c1cd601a76f8c45f403f05e57b5
MelissaCanu <148777539+MelissaCanu@users.noreply.github.com>
2024-09-30 18:14:07 +0200

    Aggiungere i file di progetto.

---
+++ WarehouseProjectApiRest/Models/Product.cs
@@ -0,0 +1,7 @@
+// Models/Product.cs
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Size { get; set; } // in qm 
+}
---
+++ WarehouseProjectApiRest/Models/ProductBatch.cs
@@ -0,0 +1,9 @@
+﻿namespace WarehouseProjectApiRest.Models
+{
+    public class ProductBatch
+    {
+        public int Id { get; set; }
+        public int Product { get; set; } // Refers to Product.Id
+        public int Quantity { get; set; }
+    }
+}
